d75789e97ab2d5dccc7b5d24bdec1e17773939c8
mtkhawaja <36654508+mtkhawaja@users.noreply.github.com>
2019-03-02 23:24:38 -0500

    Implemented metaData TextView (Rated R or Not, Release Date and original Language for movies) Changed access modifiers and scope for  activity_detail class variable. Changed theme for Detail Layout.

--- app/src/main/java/com/example/flickster/DetailActivity.java
+++ app/src/main/java/com/example/flickster/DetailActivity.java
@@ -21,32 +21,44 @@ import org.parceler.Parcels;
 import cz.msebera.android.httpclient.Header;
 
 public class DetailActivity extends YouTubeBaseActivity {
-    private TextView    title;
-    private RatingBar   rating;
-    private TextView    overview;
-    private Movie       movie;
-    private YouTubePlayerView YouTubePlayer;
+    /********************************** Instance Variables **************************************/
+
+    private YouTubePlayerView   YouTubePlayer;
     private static final String YOUTUBE_API_KEY     = "AIzaSyD6SKEJUkJofJX2HmxYNk23PQ_5CTitznA";
     private static final String TRAILERS_API_KEY    = "https://api.themoviedb.org/3/movie/%d/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+
+   //Methods
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-        // View Grabbing
-        movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra("movie"));
-        title                   = findViewById(R.id.detailMovieTitle);
-        overview                = findViewById(R.id.detailMovieOverview);
-        rating                  = findViewById(R.id.ratingBar);
-        YouTubePlayer           = findViewById(R.id.player);
 
-        // View Data Assignment
+        Movie               movie;
+        TextView            title;
+        RatingBar           rating;
+        TextView            overview;
+        TextView            metaData;
+
+        movie = Parcels.unwrap(getIntent().getParcelableExtra("movie"));
+      //View Holder References
+        title                   = findViewById(R.id.detailMovieTitle    );
+        overview                = findViewById(R.id.detailMovieOverview );
+        rating                  = findViewById(R.id.ratingBar           );
+        metaData                = findViewById(R.id.metaData            );
+        YouTubePlayer           = findViewById(R.id.player              );
+
+        // View Holder Data Assignment
+
         title.setText(movie.getTitle());
         overview.setText(movie.getOverView());
         rating.setRating((float) movie.getRating());
+        metaData.setText(movie.getMetaDataText());
 
-        // URL Grabbing
+        // JSON Handling
+
         AsyncHttpClient client = new AsyncHttpClient();
-        client.get(String.format(TRAILERS_API_KEY,movie.getmovieID()), new JsonHttpResponseHandler(){
+        client.get(String.format(TRAILERS_API_KEY,movie.getMovieID()), new JsonHttpResponseHandler(){
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 try {
@@ -55,7 +67,7 @@ public class DetailActivity extends YouTubeBaseActivity {
                        return;
                    JSONObject movieTrailer  = result.getJSONObject(0);
                    final String youtubeKey = movieTrailer.getString("key");
-                   intializeYoutube(youtubeKey);
+                   initializeYoutube(youtubeKey);
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -70,7 +82,7 @@ public class DetailActivity extends YouTubeBaseActivity {
 
     } // On-Create Method
 
-    private void intializeYoutube(String youtubeKey) {
+    private void initializeYoutube(String youtubeKey) {
         YouTubePlayer.initialize(YOUTUBE_API_KEY, new YouTubePlayer.OnInitializedListener() {
             @Override
             public void onInitializationSuccess(com.google.android.youtube.player.YouTubePlayer.Provider provider, com.google.android.youtube.player.YouTubePlayer youTubePlayer, boolean b) {
@@ -82,5 +94,6 @@ public class DetailActivity extends YouTubeBaseActivity {
                 Log.e("MovieTrailerActivity", "Error initializing YouTube player");
             }
         });
-    }
-}
+    } // initializeYoutube Method
+
+} //class
--- app/src/main/java/com/example/flickster/models/Movie.java
+++ app/src/main/java/com/example/flickster/models/Movie.java
@@ -9,33 +9,56 @@ import java.util.ArrayList;
 import java.util.List;
 @Parcel
 
+// Sample Request: https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed
+
 public class Movie {
+
+    protected  int         votes;
+    protected  Double      rating;
+    protected  int         movieID;
+    protected  boolean     isAdult;
+    protected  String      language;
+    protected  String      releaseDate;
+    protected  String      title;
+    protected  String      overView;
+    protected  String      backDropPath;
+    protected  String      posterPath;
+    protected  String      metaDataText;
+
+
     //Empty Constructor Required By Parceler Library.
-    public  Movie (){
-
-    }
-    private String posterPath;
-    private String title;
-    private String overView;
-    private String backDropPath;
-    private Double rating;
-    private int     movieID;
-
+    public  Movie (){}
 
     public Movie (JSONObject jsonObject) throws JSONException {
-        this.posterPath   =  jsonObject.getString("poster_path"   );
-        this.title        =  jsonObject.getString("title"         );
-        this.overView     =  jsonObject.getString("overview"      );
-        this.backDropPath =  jsonObject.getString("backdrop_path" );
-        this.rating       =  jsonObject.getDouble ("vote_average" );
-        this.movieID      =  jsonObject.getInt    ("id"           );
+        this.posterPath   =  jsonObject.getString ("poster_path"      );
+        this.title        =  jsonObject.getString ("title"            );
+        this.overView     =  jsonObject.getString ("overview"         );
+        this.backDropPath =  jsonObject.getString ("backdrop_path"    );
+        this.rating       =  jsonObject.getDouble ("vote_average"     );
+        this.movieID      =  jsonObject.getInt    ("id"               );
+        this.votes        =  jsonObject.getInt    ("vote_count"       );
+        this.isAdult      =  jsonObject.getBoolean("adult"            );
+        this.language     =  jsonObject.getString ("original_language");
+        this.releaseDate  =  jsonObject.getString ("release_date"     );
+        this.releaseDate  =  releaseDate.substring(5,7) + "/" +
+                             releaseDate.substring(8,10) + "/" + releaseDate.substring(0,4);
+        String ratedR;
+        if(isAdult())
+            ratedR = "Rated R ";
+        else
+            ratedR = "";
+
+        metaDataText =   ratedR + " Release Date: " + releaseDate + " Original Language:- " + language;
+
     } // constructor
+
+    /********************************** Getters **************************************/
 
     public double getRating() {
         return rating;
     }
 
-    public int getmovieID(){
+    public int getMovieID(){
         return  movieID;
     }
     public static List <Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -60,5 +83,24 @@ public class Movie {
 
     public String getBackDropPath() {
         return String.format("https://image.tmdb.org/t/p/w342/%s",backDropPath);
+    }
+
+    public int getVotes() {
+        return votes;
+    }
+
+    public boolean isAdult() {
+        return isAdult;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+    public String getMetaDataText() {
+        return metaDataText;
     }
 } //class
